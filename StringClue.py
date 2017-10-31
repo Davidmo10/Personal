e7b75e44eb50bd9920ee6a85807c1f0a43111fed
Davidmo10 <32523943+Davidmo10@users.noreply.github.com>
2017-10-30 22:32:09 -0600

    Implementation of StringClue Class

--- StringClue.py
+++ StringClue.py
@@ -2,5 +2,10 @@ from Clue import Clue
 
 
 class StringClue(Clue):
+	def __init__(self, string = None):
+	  if (string == None):
+	    raise ValueError('Must specify clue during instantiation')#Placeholder
+	  self.clue = string
 	def display(self):
-		pass
+		print(self.clue)
+

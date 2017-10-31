a89e0b557e87bb1d2f3c27b7d3ceab96eca5a6bd
Zak Sharp <macmaster195@gmail.com>
2017-10-30 21:54:39 -0500

    Changed around Landmark interfaces Went from Team interacting to Game.

--- Landmark.py
+++ Landmark.py
@@ -1,21 +1,26 @@
+from Clue import Clue
+from Confirmation import Confirmation
+from LandmarkGame import LandmarkGame
 from LandmarkGameMaker import LandmarkGameMaker
-from LandmarkTeam import LandmarkTeam
 
 
-class Landmark(LandmarkGameMaker, LandmarkTeam):
+class Landmark(LandmarkGameMaker, LandmarkGame):
 
 	clues = {}
 
 	confirmations = {}
 
-	def add_question(self):
+	def add_confirmation(self, confirmation: Confirmation):
 		pass
 
-	def add_clue(self):
+	def add_clue(self, clue: Clue):
 		pass
 
 	def get_confirmation(self):
 		pass
 
 	def get_clue(self):
+		pass
+
+	def check_answer(self, string: str) -> bool:
 		pass
--- LandmarkGameMaker.py
+++ LandmarkGameMaker.py
@@ -1,10 +1,13 @@
 from abc import ABC
 
+from Clue import Clue
+from Confirmation import Confirmation
+
 
 class LandmarkGameMaker(ABC):
 
-	def add_question(self):
+	def add_confirmation(self, confirmation: Confirmation):
 		pass
 
-	def add_clue(self):
+	def add_clue(self, clue: Clue):
 		pass

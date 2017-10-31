e8cc61bce8b28729d2b05957aa2b0d167fbe4c64
Zak Sharp <macmaster195@gmail.com>
2017-10-30 20:52:43 -0500

    Added funtion description for UserFactory

--- UserFactory.py
+++ UserFactory.py
@@ -1,5 +1,13 @@
+from User import User
+
 
 class UserFactory:
-
-	def make_user(self):
+	def make_user(self, user: str, password: str) -> User:
+		"""
+		Generates a user object based on whether the username specified is a GameMaker or a team
+		:raises: LoginError if username does not exist or password does not match username
+		:param user: Username of the user being logged into
+		:param password: Password of the user being logged into
+		:return: User object corresponding to login details
+		"""
 		pass

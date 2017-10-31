f00f98e1a0a18cd03b6c50061a07888f34c03d01
Davidmo10 <32523943+Davidmo10@users.noreply.github.com>
2017-10-30 22:00:28 -0600

    Update GameGameMaker.py

--- GameGameMaker.py
+++ GameGameMaker.py
@@ -2,14 +2,19 @@ from abc import ABC
 
 
 class GameGameMaker(ABC):
+	
+	@abc.abstractmethod
 	def start(self):
 		pass
-
+	
+	@abc.abstractmethod
 	def stop(self):
 		pass
-
+	
+	@abc.abstractmethod
 	def get_landmarks(self)->dict:
 		pass
-
+	
+	@abc.abstractmethod
 	def add_landmark(self):
 		pass

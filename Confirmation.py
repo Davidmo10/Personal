1b99922d1d7a0835eb44bc71b26ab5f58119eac6
Davidmo10 <32523943+Davidmo10@users.noreply.github.com>
2017-10-30 22:08:41 -0600

    Update Confirmation.py

--- Confirmation.py
+++ Confirmation.py
@@ -4,9 +4,9 @@ from abc import ABC
 class Confirmation(ABC):
 	
 	@abc.abstractmethod
-	def display(self):
-		pass
-	
-	@abc.abstractmethod
 	def validate(self):
+		pass
+
+	@abc.abstractmethod
+	def display(self):
 		pass

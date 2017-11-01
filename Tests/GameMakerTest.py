4abbce2226688b3bc8878dd4cd880cfd2e74efdf
Liz <32341579+cutiepatooties@users.noreply.github.com>
2017-11-01 11:39:27 -0500

    test for end_Game in GameMaker

--- Tests/GameMakerTest.py
+++ Tests/GameMakerTest.py
@@ -10,6 +10,8 @@ class TestGameMaker(unittest.TestCase):
 	def test_createGame(self):
 		self.assertTrue(self.GM.create_game, "The game has been created.")
 
+	def test_endGame(self):
+		self.assertTrue(self.GM.end_game, "The game was ended successfully")
 	# Cannot create a game, when game is happening
 	# Only GM can create game
 	# Creates only one game

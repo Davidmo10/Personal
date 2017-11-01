bc6a402521f2cd4bf1fc163ab5c5c5130a57228c
Liz <32341579+cutiepatooties@users.noreply.github.com>
2017-11-01 13:12:41 -0500

    Update TeamTest.py

--- Tests/TeamTest.py
+++ Tests/TeamTest.py
@@ -1,10 +1,13 @@
 import unittest
 from Team import Team
+from Game import Game
 
 class TestTeam(unittest.TestCase):
   
   def setUp(self):
     self.TM = Team()
+    self.GA = Game()
     
   def test_forfeit(self):
-    self.assertTrue(self.TM.forfeit, "The game was successfully forfeited");
+    if(self.assertTrue(self.GA.start))
+      self.assertTrue(self.TM.forfeit, "The game was successfully forfeited");

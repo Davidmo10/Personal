7d84f2d359b8f24288139393de68e88e359086c7
Zak Sharp <macmaster195@gmail.com>
2017-11-01 13:19:28 -0500

    Finalized parser tests

--- Tests/ParserTest.py
+++ Tests/ParserTest.py
@@ -21,8 +21,8 @@ class ParserTest(TestCase):
 		self.user = MockUser()
 
 	def test_set_user(self):
-		self.par.setUser(self.user)
-		self.assertEqual(self.par.user, self.user, "Should be able to set user")
+		self.par.parse("login user test")
+		self.assertEqual(self.par.user, self.user, "Should be able to login")
 		self.assertEqual(self.par.commandsDict, self.user.list_commands(), "setting user should set commands dict")
 
 	def test_empty_call(self):
@@ -34,11 +34,13 @@ class ParserTest(TestCase):
 		self.assertEqual(self.par.parse("arg foo"), "foo", "a command with an argument doesn't pass correctly")
 
 	def test_argument_quantity_low(self):
-		# test like last one but don't have anything after arg
+		self.par.commandsDict = self.user.list_commands()
+		self.assertEqual(self.par.parse("arg"), "Please add an argument to that command", "a command that requires an argument should alert the user they need an argument")
 		pass
 
 	def test_argument_quantity_high(self):
-		# test like last one but have many things after arg; should just ignore everything but first arg
+		self.par.commandsDict = self.user.list_commands()
+		self.assertEqual(self.par.parse("arg foo aaaaa bar"), "foo", "a command too many arguments ignores extra args")
 		pass
 
 	def test_invalid_command(self):

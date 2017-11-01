733b2001f57f0725d27fa9d0297c4f0556c8b8ba
Davidmo10 <32523943+Davidmo10@users.noreply.github.com>
2017-11-01 12:49:33 -0600

    Create TestStringQuestion.py

---
+++ Tests/TestStringQuestion.py
@@ -0,0 +1,21 @@
+import unittest
+from StringQuestion import StringQuestion
+
+
+
+class TestStringQuestion(unittest.TestCase):
+
+	def setUp(self):
+		self.Question = StringQuestion("What is five plus five?", "Ten")
+   
+  def test_initialization(self):
+    self.assertEqual(self.Question.question, "What is five plus five?")
+    self.assertEqual(self.Question.question, "Ten")
+    
+  def test_validate(self):
+    self.assertFalse(self.Question.validate("Five")
+    self.asertTrue(self.Question.validate("Ten")
+    
+  def test_display(self):
+    self.assertEqual(self.Question.display(), "What is five plus five?")
+ 

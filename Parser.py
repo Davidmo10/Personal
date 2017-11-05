a7a520b0fbcf861b06cd3d27620584252232a053
Zak Sharp <macmaster195@gmail.com>
2017-11-05 13:35:29 -0600

    Refactored; no longer using gross switch thing 6/7 tests passing

--- Parser.py
+++ Parser.py
@@ -7,23 +7,21 @@ class Parser:
 		self.commandsDict = {}
 
 	def parse(self, command: str) -> str:
+		# TODO: fix up variable names
 		args_list = command.split(" ")
 		if args_list[0] not in self.commandsDict.keys():
 			return "You can't access that command"
 
 		command = self.commandsDict[args_list[0]]
+		actual_args = args_list[1:]
 		if not callable(command):
 			return str(command)
 		required_args = signature(command).parameters
-		if (len(args_list)-1) < len(required_args):
+		if (len(actual_args)) < len(required_args):
 			return "Please add more arguments to that command"
-		if len(required_args) is 1:
-			return str(command(args_list[1]))
-		elif len(required_args) is 2:
-			return str(command(args_list[1],args_list[2]))
-		elif len(required_args) is 3:
-			return str(command(args_list[1],args_list[2],args_list[3]))
-		elif len(required_args) is 4:
-			pass
-		else:
-			return "this is a switch statement, and you've exceeded the max number of args"
+
+		user_args = []
+		for i in range(len(required_args)):
+			user_args.append(actual_args[i])
+
+		return str(command(*user_args))

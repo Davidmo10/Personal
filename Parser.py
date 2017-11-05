dcacfadf1260690db1321600c2b031414463be34
Zak Sharp <macmaster195@gmail.com>
2017-11-05 14:04:41 -0600

    Renamed variables; replaced inefficient loop with list split 6/7 tests passing

--- Parser.py
+++ Parser.py
@@ -7,21 +7,20 @@ class Parser:
 		self.commandsDict = {}
 
 	def parse(self, command: str) -> str:
-		# TODO: fix up variable names
-		args_list = command.split(" ")
-		if args_list[0] not in self.commandsDict.keys():
+		tokens = command.split(" ")
+		args_list = tokens[1:]
+
+		if tokens[0] not in self.commandsDict.keys():
 			return "You can't access that command"
 
-		command = self.commandsDict[args_list[0]]
-		actual_args = args_list[1:]
+		command = self.commandsDict[tokens[0]]
 		if not callable(command):
 			return str(command)
-		required_args = signature(command).parameters
-		if (len(actual_args)) < len(required_args):
+
+		args_required = len(signature(command).parameters)  # Number of args required for command
+		if len(args_list) < args_required:
 			return "Please add more arguments to that command"
 
-		user_args = []
-		for i in range(len(required_args)):
-			user_args.append(actual_args[i])
+		command_args = args_list[0:args_required]
 
-		return str(command(*user_args))
+		return str(command(*command_args))

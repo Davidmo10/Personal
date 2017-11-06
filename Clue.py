700d083fe43ae4a2cd09972a890f6bf516e0ecca
Lightning0 <zbsmith777@gmail.com>
2017-11-06 15:36:00 -0600

    removed bad code

--- Clue.py
+++ Clue.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 
 class Clue(ABC):
 	@abstractmethod
+
 	def display(self):
 		pass
 	
--- User.py
+++ User.py
@@ -1,14 +1,4 @@
 class User():
 
 	def __init__(self, user_id):
-		self.team_id = user_id
-		self.db = Team_DBMock()
-		try:
-			info = self.db.fetch_info(user_id)
-		except InvalidUserException:
-			pass
-		self.name = info.name
-		self.game_id = info.game_id
-		self.events = self.db.fetch_status(self.game_id, self.team_id)
-
-	def
+		pass

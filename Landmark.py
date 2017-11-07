d22735281934a2654089dafa47c43dd47e67cb3c
Davidmo10 <32523943+Davidmo10@users.noreply.github.com>
2017-11-07 17:34:57 -0600

    Spacing and init
    constructor no longer requires landmarkgame and landmarkgamemaker

--- Landmark.py
+++ Landmark.py
@@ -1,30 +1,26 @@
 from Clue import Clue
-# from StringQuestion import StringQuestion
 from Confirmation import Confirmation
 from LandmarkGame import LandmarkGame
 from LandmarkGameMaker import LandmarkGameMaker
 
 
-class Landmark(LandmarkGameMaker, LandmarkGame):
+class Landmark():
+    def __init__(self, name):
+        self.clue = None
+        self.confirmation = None
+        self.name = name
 
-	def __init__(self, name):
-		self.clue = None
-		self.confirmation = None
-		self.name = name
+    def add_confirmation(self, confirmation: Confirmation):
+        self.confirmation = confirmation
 
-	def set_confirmation(self, confirmation: Confirmation) -> bool:
-		self.confirmation = confirmation
-		return True
+    def add_clue(self, clue: Clue):
+        self.clue = clue
 
-	def set_clue(self, clue: Clue) -> bool:
-		self.clue = clue
-		return True
+    def get_confirmation(self):
+        return self.confirmation
 
-	def get_confirmation(self) -> Confirmation:
-		return self.confirmation
+    def get_clue(self):
+        return self.clue
 
-	def get_clue(self) -> Clue:
-		return self.clue
-
-	def check_answer(self, string: str) -> bool:
-		return self.get_confirmation() == string
+    def check_answer(self, string: str) -> bool:
+        return self.confirmation.get("Confirmation").validate(str)

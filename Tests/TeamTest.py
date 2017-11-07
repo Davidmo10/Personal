5a5ad4c333fd2d2aadf9310054067129dc163a61
Davidmo10 <32523943+Davidmo10@users.noreply.github.com>
2017-11-07 17:36:59 -0600

    Fixed spacing issues from pycharm and some more
    self.team.landmark is now self.team.landmark_index
    self.TM = Team(self.GA) (correct with current implementation of Team()

--- Tests/TeamTest.py
+++ Tests/TeamTest.py
@@ -7,50 +7,48 @@ from Team import Team
 
 
 class TestTeamConfirmation(unittest.TestCase):
+    def setUp(self):
+        self.string_question = StringQuestion("What color is the car?", "Red")
+        self.landmark = Landmark("Landmark One")
+        self.landmark.confirmation["Confirmation"] = self.string_question
 
-	def setUp(self):
-		self.string_question = StringQuestion("What color is the car?", "Red")
-		self.landmark = Landmark()
-		self.landmark.confirmations["Confirmation"] = self.string_question
+        self.game = Game()
+        self.game.landmarkList[0] = self.landmark
 
-		self.game = Game()
-		self.game.landmarkList[0] = self.landmark
+        self.team = Team(self.game)
+        self.team.game = self.game  # This conflicts with the constructor. Choose to set game one place or the other
 
-		self.team = Team()
-		self.team.game = self.game
+    def test_getQuestion(self):
+        self.assertEqual(
+            self.team.request_question(), "What color is the car?", "Returned: " + self.landmark.get_confirmation()
+                                                                    + " instead of proper question")
 
-	def test_getQuestion(self):
-		self.assertEqual(
-			self.team.request_question(), "What color is the car?", "Returned: " + self.landmark.get_confirmation()
-			+ " instead of proper question")
+    def test_answerWithoutGetQuestion(self):
+        with self.assertRaises(Exception):
+            self.team.answer("Red")
 
-	def test_answerWithoutGetQuestion(self):
-		with self.assertRaises(Exception):
-			self.team.answer("Red")
+    def test_answerCorrect(self):
+        self.assertTrue(self.team.answer("Red"), "Did not accept correct answer")
 
-	def test_answerCorrect(self):
-		self.assertTrue(self.team.answer("Red"), "Did not accept correct answer")
+    def test_answerIncorrect(self):
+        self.assertFalse(self.team.answer("Blue"), "Accepted incorrect answer")
 
-	def test_answerIncorrect(self):
-		self.assertFalse(self.team.answer("Blue"), "Accepted incorrect answer")
+    def test_landmarkIdAfterIncorrect(self):
+        self.team.request_question()
+        self.team.answer("Blue")
+        self.assertEqual(self.team.landmark_index, 0, "Landmark id was incremented despite incorrect answer")
 
-	def test_landmarkIdAfterIncorrect(self):
-		self.team.request_question()
-		self.team.answer("Blue")
-		self.assertEqual(self.team.landmark, 0, "Landmark id was incremented despite incorrect answer")
+    def test_landmarkIdAfterCorrect(self):
+        self.team.request_question()
+        self.team.answer("Red")
+        self.assertEqual(self.team.landmark_index, 1, "Landmark id was not incremented despite correct answer")
 
-	def test_landmarkIdAfterCorrect(self):
-		self.team.request_question()
-		self.team.answer("Red")
-		self.assertEqual(self.team.landmark, 1, "Landmark id was not incremented despite correct answer")
-		
 
 class TestTeam(unittest.TestCase):
-
-	def setUp(self):
-		self.TM = Team()
-		self.GA = Game()
-
-	def test_forfeit(self):
-		if self.assertTrue(self.GA.start):
-			self.assertTrue(self.TM.forfeit, "The game was successfully forfeited")
+    def setUp(self):
+        #self.TM = Team()
+        self.GA = Game()
+	self.TM = Team(self.GA)
+    def test_forfeit(self):
+        if self.assertTrue(self.GA.start):
+            self.assertTrue(self.TM.forfeit, "The game was successfully forfeited")

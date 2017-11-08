bd670a10c46f17361ea473e8742ccaf22aaf2600
Joseph Yang <yang445@uwm.edu>
2017-11-08 14:13:40 -0600

    Added test for invalid command, moved login tests to new class

--- Tests/Test_ParserAcceptance.py
+++ Tests/Test_ParserAcceptance.py
@@ -3,40 +3,47 @@ from Parser import Parser
 from Game import Game
 
 
+class ParserAcceptanceLogin(unittest.TestCase):
+
+	def setup(self):
+		self.parser = Parser(Game())
+
+	def test_gamemaker_login(self):
+		self.assertEqual(self.parser.parse("login maker password"), "Logged in!", "Did not log in")
+
+	def test_team_login(self):
+		self.parser.parse("login maker password")
+		self.parser.parse("createteam TeamName TeamPass")
+		self.parser.parse("logout")
+		self.assertEqual(self.parser.parse("login TeamName TeamPass"), "Logged in!", "Did not log in")
+
+
 class ParserAcceptanceGameMaker(unittest.TestCase):
 
 	def setUp(self):
 		self.parser = Parser(Game())
-
-	def test_login(self):
-		self.assertEqual(self.parser.parse("login maker password"), "Logged in!", "Did not log in")
+		self.parser.parse("login maker password")
 
 	def test_logout(self):
-		self.parser.parse("login maker password")
 		self.assertEqual(self.parser.parse("logout"), "Success!", "Did not log out")
 
 	def test_create_team(self):
-		self.parser.parse("login maker password")
 		self.assertEqual(self.parser.parse("createteam TestTeam SecretPassword"), "Success!", "Did not create team")
 
 	def test_create_landmark(self):
-		self.parser.parse("login maker password")
 		self.assertEqual(self.parser.parse("createlandmark TestLandmark"), "Success!", "Did not create landmark")
 
 	def test_edit_landmark_clue(self):
-		self.parser.parse("login maker password")
 		self.parser.parse("createlandmark TestLandmark")
 		self.assertEqual(self.parser.parse("landmarkclue TestLandmark TestClue"), "Success!", "Did not create/edit clue")
 
 	def test_edit_landmark_question(self):
-		self.parser.parse("login maker password")
 		self.parser.parse("createlandmark TestLandmark")
 		self.assertEqual(
 			self.parser.parse("landmarkquestion TestLandmark TestQuestion TestAnswer"), "Success!", "Did not create/edit"
 			" question")
 
 	def test_start_game(self):
-		self.parser.parse("login maker password")
 		self.assertEqual(self.parser.parse("startgame"), "Success!", "Did not start game")
 
 	# def test_list_commands(self):
@@ -44,7 +51,6 @@ class ParserAcceptanceGameMaker(unittest.TestCase):
 	# 	 self.parser.parse("commands")
 
 	def test_end_game(self):
-		self.parser.parse("login maker password")
 		self.parser.parse("startgame")
 		self.assertEqual(self.parser.parse("endgame"), "Success!", "did not end game")
 
@@ -74,23 +80,21 @@ class ParserAcceptanceTeam(unittest.TestCase):
 		self.parser.parse("startgame")
 		self.parser.parse("logout")
 
+		self.parser.parse("login TeamName TeamPass")
+
 	def test_login(self):
 		self.assertEqual(self.parser.parse("login TeamName TeamPass"), "Logged in!", "Did not log in")
 
 	def test_logout(self):
-		self.parser.parse("login TeamName TeamPass")
 		self.assertEqual(self.parser.parse("logout"), "Success!", "Did not log out")
 
 	def test_request_clue(self):
-		self.parser.parse("login TeamName TeamPass")
 		self.assertEqual(self.parser.parse("getclue"), "TestClue", "Did not return clue")
 
 	def test_request_question(self):
-		self.parser.parse("login TeamName TeamPass")
 		self.assertEqual(self.parser.parse("getquestion"), "TestQuestion", "Did not return question")
 
 	def test_answer(self):
-		self.parser.parse("login TeamName TeamPass")
 		self.parser.parse("getquestion")
 		self.assertEqual(self.parser.parse("answer TestAnswer"), "Success!", "Did not process answering")
 
@@ -100,5 +104,4 @@ class ParserAcceptanceTeam(unittest.TestCase):
 	# 	self.parser.parse("listcommands")
 
 	def test_forfeit(self):
-		self.parser.parse("login TeamName TeamPass")
 		self.assertEqual(self.parser.parse("forfeit"), "Success!", "Did not forfeit game")

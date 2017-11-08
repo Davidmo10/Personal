539501d7694c4c6b17adcac3797d2e5e3b784db7
Zak Sharp <macmaster195@gmail.com>
2017-11-07 20:38:07 -0600

    Added commands list to GameMaker Cleaned up GameMaker a bit

--- GameMaker.py
+++ GameMaker.py
@@ -8,7 +8,12 @@ from Game import Game
 class GameMaker(User):
 	def __init__(self, game: Game):
 		super().__init__()
-		self.commands = {1: "logout(self)", 2: "list_commands(self)"}  # dict
+		self.commands = {"crlm": lambda name: self.create_landmark(name),
+		                 "crtm": lambda name, password: self.create_team(name,password),
+		                 "edlmcl": lambda name, clue: self.edit_landmark_clue(name, clue),
+		                 "edlmq": lambda name, question, answer: self.edit_landmark_question(name, question, answer),
+		                 "end": lambda: self.end_game(),
+		                 "lmks": lambda: self.list_landmarks()}  # dict
 		self.password = None
 		self.name = None
 		self.myGame = game
@@ -16,7 +21,6 @@ class GameMaker(User):
 	# # User Inheritance:
 	# def login(self, password, name) -> bool:
 	# 	return self.password == password and self.name == name
-
 	def list_commands(self) -> dict:
 		return self.commands
 
@@ -30,7 +34,7 @@ class GameMaker(User):
 	# 	self.myGame.start();
 	# 	return self.myGame
 
-	def create_landmark(self, name) -> bool:
+	def create_landmark(self, name: str) -> bool:
 		temp_landmark = Landmark(name)
 
 		# temp_landmark.add_confirmation(question, answer)
@@ -39,26 +43,25 @@ class GameMaker(User):
 		self.myGame.landmarkList.append(temp_landmark)
 		return True
 
-	def edit_landmark_clue(self, name, new_clue: str) -> bool:
-		tempClue = StringClue(new_clue)
-		self.myGame.get_landmark_by_name(name).set_clue(tempClue)
+	def edit_landmark_clue(self, name: str, new_clue: str) -> bool:
+		temp_clue = StringClue(new_clue)
+		self.myGame.get_landmark_by_name(name).set_clue(temp_clue)
 		return True
 
-	def edit_landmark_question(self, name, new_question, new_answer) -> bool:
-		tempQuestion = StringQuestion(new_question, new_answer)
-		self.myGame.get_landmark_by_name(name).set_confirmation(tempQuestion)
+	def edit_landmark_question(self, name: str, new_question: str, new_answer: str) -> bool:
+		temp_question = StringQuestion(new_question, new_answer)
+		self.myGame.get_landmark_by_name(name).set_confirmation(temp_question)
 		return True
 
 	def list_landmarks(self) -> str:
-		stringlist = None
+		string_list = None
 		for i in self.myGame.landmarkList:
-			stringlist += ''.join(self.myGame.landmarkList[i].name)
-		return stringlist
+			string_list += ''.join(self.myGame.landmarkList[i].name)
+		return string_list
 
-
-	# def create_team(self, name) -> bool:
-	# 	self.temp_user = User
-
+	def create_team(self, name: str, password: str) -> bool:
+		temp_user = User
+		return False
 
 	# def create_scavenger_hunt(self):
 	# 	pass

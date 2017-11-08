a35d3cd7e70dc29be64643f0bacee986fc1909ea
Lightning0 <zbsmith777@gmail.com>
2017-11-07 21:08:20 -0600

    Updaed TeamTest

--- Tests/TeamTest.py
+++ Tests/TeamTest.py
@@ -16,16 +16,16 @@ class TestTeamConfirmation(unittest.TestCase):
 		self.tempQuestion = StringQuestion("question", "answer")
 		self.tempClue = StringClue("clue")
 		self.landmark.confirmation = self.tempQuestion
-		self.game.landmarkList[0] = self.landmark
-		self.team = Team(self.game)
-		self.team.game = self.game  # This conflicts with the constructor. Choose to set game one place or the other
+		self.MyGame.landmarkList[0] = self.landmark
+		self.team = Team(self.MyGame)
+		self.team.game = self.MyGame  # This conflicts with the constructor. Choose to set game one place or the other
 
 	def test_getQuestion(self):
 		self.assertEqual(self.myTeam.request_question(), self.tempQuestion, "Can not retrieve proper Question ")
 
 	def test_answerWithoutGetQuestion(self):
 		with self.assertRaises(Exception):
-			self.myTeam.answer()
+			self.myTeam.answer("something")
 
 	def test_answerCorrect(self):
 		self.assertTrue(self.team.answer("Red"), "Did not accept correct answer")
@@ -46,9 +46,9 @@ class TestTeamConfirmation(unittest.TestCase):
 
 class TestTeam(unittest.TestCase):
 	def setUp(self):
-		#self.TM = Team()
 		self.GA = Game()
-	self.TM = Team(self.GA)
+		self.TM = Team(self.GA)
+
 	def test_forfeit(self):
 		if self.assertTrue(self.GA.start):
 			self.assertTrue(self.TM.forfeit, "The game was successfully forfeited")

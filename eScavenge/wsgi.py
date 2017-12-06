5f5f94d3811736d9cbea3a0ebb023fdc3fe526d2
Lightning0 <zbsmith777@gmail.com>
2017-12-05 19:16:15 -0600

    Tests

--- eScavenge/wsgi.py
+++ eScavenge/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eScavenge.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'eScavenge.settings'
 
 application = get_wsgi_application()

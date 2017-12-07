311fc87d522e88701d9d6180c70da0c2846f0439
SubAdam <32341846+SubAdam@users.noreply.github.com>
2017-12-06 23:28:17 -0600

    update
    finished updating acceptance

--- web/Test_Acceptance.py
+++ web/Test_Acceptance.py
@@ -60,44 +60,58 @@ class GameMakerTests(TestCase):
 		pass
 
 	def test_make_score_scheme(self):
+		#as a Game Maker I can successfully add a scoring scheme to game
 		pass
 
 	def test_edit_score_scheme(self):
+		#as a Game Maker I can successfully edit a scorring scheme of game
 		pass
 
 	def test_request_status(self):
+		#as a Game Maker I can get the status of a current game
 		pass
 
 	def test_edit_game_name_and_description(self):
+		#as a Game Maker I can successfully edit name of game
+		#as a Game Maker I can successfully edit description of game
 		pass
 
 	def test_start_game(self):
+		#as a Game Maker I can set game staus to on
 		pass
 
 	def test_stop_game(self):
+		#as a Game Maker I can set game status to off
 		pass
 
 	def test_game_is_on(self):
+		#as a Game Maker I can check the status of a game
 		pass
 
 
 class TeamTests(TestCase):
 
 	def test_request_clue(self):
-		#as 
+		#as a Team I can successfully request a clue
 		pass
 
 	def test_request_question(self):
+		#as a Team I can successfully request a question
 		pass
 
 	def test_submit_answer(self):
+		#as a Team I can successfully submit an answer
 		pass
 
 	def test_edit_credentials(self):
+		#as a Team I can edit username
+		#as a Team I can edit password
 		pass
 
 	def test_forfeit(self):
+		#as a Team I can forfeit the game, removing team from game
 		pass
 
 	def test_is_on(self):
+		#as a Team I can check if on
 		pass

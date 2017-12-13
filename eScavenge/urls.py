61dd2dcf7b0385705696133664d0d434ae5749cf
Zak Sharp <macmaster195@gmail.com>
2017-12-12 18:10:16 -0600

    Changed up the link syntax to be happy with Django. It did work before, but this is more extensible
    An explanation:
    (Using line 16)
    Django assumed "/do/logout" was a directory. It isn't
    {% url %} is a call to the function url.
    If it has one string argument, it does a lookup by the name argument
    The type="logout" just takes the place of that second argument
    
    Signed-off-by: Zak Sharp <macmaster195@gmail.com>

--- eScavenge/urls.py
+++ eScavenge/urls.py
@@ -13,21 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
-from web import views
-
 from django.conf import settings
 from django.conf.urls import include, url
+from django.contrib import admin
 
+from web import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login$', views.login),
     url(r'^$', views.dash),
-    url(r'^req/(?P<type>.*)', views.req),
-    url(r'^do/(?P<type>.*)', views.do),
-    url(r'^edit/(?P<type>.*)', views.edit)
+    url(r'^req/(?P<type>.*)', views.req, name="req"),
+    url(r'^do/(?P<type>.*)', views.do, name="do"),
+    url(r'^edit/(?P<type>.*)', views.edit, name="edit")
 ]
 if settings.DEBUG:
     import debug_toolbar
